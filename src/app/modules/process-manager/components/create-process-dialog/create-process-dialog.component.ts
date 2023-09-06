47a97ae689280fe9abeb8310e1bee758fce950c1
Arthur Maia <arthur.maia.dev@gmail.com>
2023-09-05 22:04:11 -0300

    added input validation based on selected scaling type

--- src/app/modules/process-manager/components/create-process-dialog/create-process-dialog.component.ts
+++ src/app/modules/process-manager/components/create-process-dialog/create-process-dialog.component.ts
@@ -1,15 +1,19 @@
-import { Component, ElementRef, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
 	MAT_DIALOG_DATA,
 	MatDialog,
 	MatDialogRef,
 } from '@angular/material/dialog';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
 import { ProcessColors } from 'src/app/shared/constants/process-colors.constants';
 import {
 	ProcessTypes,
 	ProcessTypesNames,
 } from 'src/app/shared/constants/process-types.constants';
+import { ScalingTypesEnum } from 'src/app/shared/constants/scaling-types.constants';
+import { ProcessesState } from 'src/app/shared/stores/processes/processes.state';
 import { ColorPickerDialogComponent } from '../color-picker-dialog/color-picker-dialog.component';
 
 @Component({
@@ -17,7 +21,10 @@ import { ColorPickerDialogComponent } from '../color-picker-dialog/color-picker-
 	templateUrl: './create-process-dialog.component.html',
 	styleUrls: ['./create-process-dialog.component.scss'],
 })
-export class CreateProcessDialogComponent {
+export class CreateProcessDialogComponent implements OnInit {
+	@Select(ProcessesState.getCurrentScalingType)
+	currentScalingType$!: Observable<ScalingTypesEnum>;
+
 	processForm: FormGroup;
 	maxProcesses = 15;
 	maxAvailableProcesses = this.maxProcesses;
@@ -35,6 +42,7 @@ export class CreateProcessDialogComponent {
 			value: ProcessTypes.cpuAndIoBound,
 		},
 	];
+	scalingType!: ScalingTypesEnum;
 
 	constructor(
 		public dialogRef: MatDialogRef<CreateProcessDialogComponent>,
@@ -56,6 +64,10 @@ export class CreateProcessDialogComponent {
 		});
 	}
 
+	get isScalingTypeCircular(): boolean {
+		return this.scalingType === ScalingTypesEnum.Circular;
+	}
+
 	pickColor() {
 		const dialogRef = this.dialog.open(ColorPickerDialogComponent, {
 			width: '484px',
@@ -71,6 +83,10 @@ export class CreateProcessDialogComponent {
 
 	onClose() {
 		this.dialogRef.close();
+	}
+
+	ngOnInit(): void {
+		this.currentScalingType$.subscribe((value) => (this.scalingType = value));
 	}
 
 	onSubmit() {
