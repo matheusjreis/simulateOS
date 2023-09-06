6cde631d0dc3d4c6d895610b208efca2ff8bfac2
Arthur Maia <arthur.maia.dev@gmail.com>
2023-09-05 22:03:18 -0300

    only refactor

--- src/app/core/shell/shell.module.ts
+++ src/app/core/shell/shell.module.ts
@@ -1,15 +1,35 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShellComponent } from './shell.component';
-import { ShellRoutingModule } from './shell-routing.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { ProcessesStatsComponent } from './components/processes-stats/processes-stats.component';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatSliderModule } from '@angular/material/slider';
-import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PickScalingTypeDialogComponent } from './components/pick-scaling-type-dialog/pick-scaling-type-dialog.component';
+import { ProcessesStatsComponent } from './components/processes-stats/processes-stats.component';
+import { ShellRoutingModule } from './shell-routing.module';
+import { ShellComponent } from './shell.component';
 
 @NgModule({
-  imports: [CommonModule, ShellRoutingModule, MatButtonModule, MatSliderModule, FormsModule],
-  declarations: [ShellComponent, NavbarComponent, ProcessesStatsComponent],
+	imports: [
+		CommonModule,
+		ShellRoutingModule,
+		MatButtonModule,
+		MatSliderModule,
+		FormsModule,
+		MatDialogModule,
+		ReactiveFormsModule,
+		MatSelectModule,
+		MatFormFieldModule,
+	],
+	declarations: [
+		ShellComponent,
+		NavbarComponent,
+		ProcessesStatsComponent,
+		PickScalingTypeDialogComponent,
+	],
 })
 export class ShellModule {}
