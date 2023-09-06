8103251575478880f230957a96b48fe39c66faea
Arthur Maia <arthur.maia.dev@gmail.com>
2023-09-05 22:03:07 -0300

    added modal button callback

--- src/app/core/shell/components/processes-stats/processes-stats.component.ts
+++ src/app/core/shell/components/processes-stats/processes-stats.component.ts
@@ -1,76 +1,92 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Select, Store } from "@ngxs/store";
-import { Observable, Subscription } from "rxjs";
-import { Process } from "src/app/shared/models/process";
-import { Processes } from "src/app/shared/stores/processes/processes.actions";
-import { ProcessesState } from "src/app/shared/stores/processes/processes.state";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSliderChange } from '@angular/material/slider';
+import { Select, Store } from '@ngxs/store';
+import { Observable, Subscription } from 'rxjs';
+import { ScalingTypesEnum } from 'src/app/shared/constants/scaling-types.constants';
+
+import { Process } from 'src/app/shared/models/process';
+import { Processes } from 'src/app/shared/stores/processes/processes.actions';
+import { ProcessesState } from 'src/app/shared/stores/processes/processes.state';
+import { PickScalingTypeDialogComponent } from '../pick-scaling-type-dialog/pick-scaling-type-dialog.component';
 
 @Component({
-    selector: "app-processes-stats",
-    templateUrl: "./processes-stats.component.html",
-    styleUrls: ["./processes-stats.component.scss"],
+	selector: 'app-processes-stats',
+	templateUrl: './processes-stats.component.html',
+	styleUrls: ['./processes-stats.component.scss'],
 })
 export class ProcessesStatsComponent implements OnInit, OnDestroy {
-    @Select(ProcessesState.getAvailableProcesses)
-    availableProcesses$!: Observable<Process[]>;
-    @Select(ProcessesState.getTimer) timer$!: Observable<number>;
-    @Select(ProcessesState.getIOWaitTime) ioWaitTime$!: Observable<number>;
-    @Select(ProcessesState.getTimeSlice) timeSlice$!: Observable<number>;
-    @Select(ProcessesState.getCpuClock) cpuClock$!: Observable<number>;
-    private subscriptions: Subscription = new Subscription();
-    ioWaitTime?: number;
-    timeSlice?: number;
-    cpuClock?: number;
+	@Select(ProcessesState.getAvailableProcesses)
+	availableProcesses$!: Observable<Process[]>;
+	@Select(ProcessesState.getTimer) timer$!: Observable<number>;
+	@Select(ProcessesState.getIOWaitTime) ioWaitTime$!: Observable<number>;
+	@Select(ProcessesState.getTimeSlice) timeSlice$!: Observable<number>;
+	@Select(ProcessesState.getCpuClock) cpuClock$!: Observable<number>;
+	private subscriptions: Subscription = new Subscription();
+	ioWaitTime?: number;
+	timeSlice?: number;
+	cpuClock?: number;
 
-    constructor(private store: Store) {}
+	constructor(private store: Store, private readonly dialog: MatDialog) {}
 
-    ngOnInit() {
-        this.startTimer();
+	startTimer() {
+		setInterval(() => {
+			this.store.dispatch(new Processes.IncrementTimer());
+		}, 1000);
+	}
 
-        this.subscriptions.add(
-            this.ioWaitTime$.subscribe((ioWaitTime: number) => {
-                this.ioWaitTime = ioWaitTime;
-            })
-        );
+	stopProcesses() {
+		this.store.dispatch(new Processes.StopProcesses());
+	}
 
-        this.subscriptions.add(
-            this.timeSlice$.subscribe((timeSlice: number) => {
-                this.timeSlice = timeSlice;
-            })
-        );
+	changeIOWaitTime(event: MatSliderChange) {
+		this.store.dispatch(new Processes.SetIOWaitTime(event.value!));
+	}
 
-        this.subscriptions.add(
-            this.cpuClock$.subscribe((cpuClock: number) => {
-                this.cpuClock = cpuClock;
-            })
-        );
-        
-        this.store.dispatch(new Processes.StartIOTimer());
-    }
+	changeTimeSlice(event: MatSliderChange) {
+		this.store.dispatch(new Processes.SetTimeSlice(event.value!));
+	}
 
-    ngOnDestroy() {
-        this.subscriptions.unsubscribe();
-    }
+	changeCpuClock(event: MatSliderChange) {
+		this.store.dispatch(new Processes.SetCpuClock(event.value!));
+	}
 
-    startTimer() {
-        setInterval(() => {
-            this.store.dispatch(new Processes.IncrementTimer());
-        }, 1000);
-    }
+	handleOpenSelectScalingType() {
+		const dialogRef = this.dialog.open(PickScalingTypeDialogComponent, {
+			width: '600px',
+			disableClose: true,
+		});
 
-    stopProcesses() {
-        this.store.dispatch(new Processes.StopProcesses());
-    }
+		dialogRef.afterClosed().subscribe((res?: ScalingTypesEnum) => {
+			if (res) {
+				this.store.dispatch(new Processes.PickScalingType(res));
+			}
+		});
+	}
 
-    changeIOWaitTime(event: any) {
-        this.store.dispatch(new Processes.SetIOWaitTime(event.value));
-    }
+	ngOnInit() {
+		this.startTimer();
 
-    changeTimeSlice(event: any) {
-        this.store.dispatch(new Processes.SetTimeSlice(event.value));
-    }
+		this.subscriptions.add(
+			this.ioWaitTime$.subscribe(
+				(ioWaitTime: number) => (this.ioWaitTime = ioWaitTime)
+			)
+		);
 
-    changeCpuClock(event: any) {
-        this.store.dispatch(new Processes.SetCpuClock(event.value));
-    }
+		this.subscriptions.add(
+			this.timeSlice$.subscribe(
+				(timeSlice: number) => (this.timeSlice = timeSlice)
+			)
+		);
+
+		this.subscriptions.add(
+			this.cpuClock$.subscribe((cpuClock: number) => (this.cpuClock = cpuClock))
+		);
+
+		this.store.dispatch(new Processes.StartIOTimer());
+	}
+
+	ngOnDestroy() {
+		this.subscriptions.unsubscribe();
+	}
 }
