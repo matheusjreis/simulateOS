9643b605a5c3e018667f860668dc6f485a5fc80a
Arthur Maia <arthur.maia.dev@gmail.com>
2023-09-12 20:02:35 -0300

    only prettier

--- src/app/shared/constants/process-states.constants.ts
+++ src/app/shared/constants/process-states.constants.ts
@@ -1,37 +1,43 @@
-export type ProcessStatesType = 'ready' | 'execution' | 'suspended' | 'io' | 'readyIo' | 'finished';
+export type ProcessStatesType =
+	| 'ready'
+	| 'execution'
+	| 'suspended'
+	| 'io'
+	| 'readyIo'
+	| 'finished';
 
 export interface IProcessStates {
-    ready: ProcessStatesType;
-    execution: ProcessStatesType;
-    suspended: ProcessStatesType;
-    io: ProcessStatesType;
+	ready: ProcessStatesType;
+	execution: ProcessStatesType;
+	suspended: ProcessStatesType;
+	io: ProcessStatesType;
 	readyIo: ProcessStatesType;
-    finished: ProcessStatesType;
+	finished: ProcessStatesType;
 }
 
 export const ProcessStates: IProcessStates = {
-    ready: 'ready',
-    execution: 'execution',
-    suspended: 'suspended',
-    io: 'io',
+	ready: 'ready',
+	execution: 'execution',
+	suspended: 'suspended',
+	io: 'io',
 	readyIo: 'readyIo',
-    finished: 'finished'
+	finished: 'finished',
 };
 
 export const ProcessStatesNames = {
-    ready: 'Pronto',
-    execution: 'Execução',
-    suspended: 'Suspenso',
-    io: 'I/O',
+	ready: 'Pronto',
+	execution: 'Execução',
+	suspended: 'Suspenso',
+	io: 'I/O',
 	readyIo: 'Pronto I/O',
-    finished: 'finished'
+	finished: 'finished',
 };
 
 export const ProcessStatesColors = {
-    ready: 'primary',
-    execution: 'accent',
-    suspended: 'warn',
-    io: 'primary',
+	ready: 'primary',
+	execution: 'accent',
+	suspended: 'warn',
+	io: 'primary',
 	readyIo: 'primary',
-    finished: 'finished'
+	finished: 'finished',
 };
