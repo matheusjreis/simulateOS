b95ea33aeaa6937740255c76d9f353f3c7cad484
Arthur Maia <arthur.maia.dev@gmail.com>
2023-09-14 09:20:03 -0300

    added some modules

--- src/app/modules/process-manager/process-manager.module.ts
+++ src/app/modules/process-manager/process-manager.module.ts
@@ -1,45 +1,52 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ProcessManagerComponent } from "./process-manager.component";
-import { ProcessManagerRoutingModule } from "./process-manager-routing.module";
-import { MatIconModule } from "@angular/material/icon";
-import { MatTableModule } from "@angular/material/table";
-import { MatChipsModule } from "@angular/material/chips";
-import { MatButtonModule } from "@angular/material/button";
-import { CreateProcessDialogComponent } from "./components/create-process-dialog/create-process-dialog.component";
-import { EditProcessDialogComponent } from "./components/edit-process-dialog/edit-process-dialog.component";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatSelectModule } from "@angular/material/select";
-import { ColorPickerDialogComponent } from "./components/color-picker-dialog/color-picker-dialog.component";
-import { MatMenuModule } from "@angular/material/menu";
-import { SharedModule } from "src/app/shared/shared.module";
-import { UpdatePriorityDialogComponent } from "./components/update-priority-dialog/update-priority-dialog.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ColorPickerDialogComponent } from './components/color-picker-dialog/color-picker-dialog.component';
+import { CreateProcessDialogComponent } from './components/create-process-dialog/create-process-dialog.component';
+import { EditProcessDialogComponent } from './components/edit-process-dialog/edit-process-dialog.component';
+import { ProcessLifetimeDialogComponent } from './components/process-lifetime-dialog/process-lifetime-dialog.component';
+import { UpdatePriorityDialogComponent } from './components/update-priority-dialog/update-priority-dialog.component';
+import { ProcessManagerRoutingModule } from './process-manager-routing.module';
+import { ProcessManagerComponent } from './process-manager.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ProcessManagerRoutingModule,
+	imports: [
+		CommonModule,
+		ProcessManagerRoutingModule,
 		MatIconModule,
-        MatTableModule,
-        MatChipsModule,
-        MatButtonModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatDialogModule,
-        MatSelectModule,
-        MatMenuModule,
-        SharedModule,
-    ],
-    declarations: [
-        ProcessManagerComponent,
-        CreateProcessDialogComponent,
-        EditProcessDialogComponent,
-        ColorPickerDialogComponent,
-        UpdatePriorityDialogComponent,
-    ],
+		MatTableModule,
+		MatChipsModule,
+		MatButtonModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatDialogModule,
+		MatSelectModule,
+		MatMenuModule,
+		SharedModule,
+		MatCheckboxModule,
+		NgApexchartsModule,
+	],
+	declarations: [
+		ProcessManagerComponent,
+		CreateProcessDialogComponent,
+		EditProcessDialogComponent,
+		ColorPickerDialogComponent,
+		UpdatePriorityDialogComponent,
+		ProcessLifetimeDialogComponent,
+	],
 })
 export class ProcessManagerModule {}
