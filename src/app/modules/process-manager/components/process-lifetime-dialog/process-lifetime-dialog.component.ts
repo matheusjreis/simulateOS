68d922828b561466a6138e88c483b1ce04fb4585
Arthur Maia <arthur.maia.dev@gmail.com>
2023-09-25 22:13:58 -0300

    added scrollToBottom + row label's color

--- src/app/modules/process-manager/components/process-lifetime-dialog/process-lifetime-dialog.component.ts
+++ src/app/modules/process-manager/components/process-lifetime-dialog/process-lifetime-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Select } from '@ngxs/store';
@@ -52,6 +53,7 @@ export class ProcessLifetimeDialogComponent implements OnInit, OnDestroy {
 	chartOptions: ChartOptions | null = null;
 
 	constructor(
+		@Inject(DOCUMENT) private readonly document: Document,
 		private readonly dialogRef: MatDialogRef<ProcessLifetimeDialogComponent>
 	) {}
 
@@ -80,6 +82,31 @@ export class ProcessLifetimeDialogComponent implements OnInit, OnDestroy {
 		);
 	}
 
+	private scrollDialogContainerToBottom(): void {
+		const dialogContainer = this.document.querySelector('.mat-dialog-content');
+
+		if (!dialogContainer) return;
+
+		dialogContainer.scrollTo({
+			top: dialogContainer.scrollHeight,
+			behavior: 'smooth',
+		});
+	}
+
+	private changedChartLabels(colors: Array<string>): void {
+		const yAxisLabelContainer = this.document.querySelector(
+			'.apexcharts-yaxis-texts-g'
+		);
+
+		if (!yAxisLabelContainer) return;
+
+		Array.from(yAxisLabelContainer.children).forEach(
+			(child: any, index: number) => {
+				child.style.fill = colors[index];
+			}
+		);
+	}
+
 	generateChart(): void {
 		const checkedProcesses = this.finishedProcesses.filter(
 			({ checked }) => checked
@@ -104,6 +131,14 @@ export class ProcessLifetimeDialogComponent implements OnInit, OnDestroy {
 			[]
 		);
 
+		const labelsColors = logsByPID.map((logs) => logs[0].process.color);
+
+		const sortedLogs = [...filteredLogs].sort(
+			(a, b) => a.currentTime - b.currentTime
+		);
+
+		const minTime = sortedLogs[0].currentTime;
+
 		const data: Array<{
 			x: string;
 			y: Array<number>;
@@ -115,8 +150,8 @@ export class ProcessLifetimeDialogComponent implements OnInit, OnDestroy {
 			const logsHalfSize = Math.floor(logs.length / 2);
 
 			for (let i = 0; i < logsHalfSize; i++) {
-				const start = logs[i * 2].currentTime;
-				const end = logs[i * 2 + 1].currentTime;
+				const start = logs[i * 2].currentTime - minTime;
+				const end = logs[i * 2 + 1].currentTime - minTime;
 
 				const process = logs[i * 2].process;
 
@@ -127,6 +162,8 @@ export class ProcessLifetimeDialogComponent implements OnInit, OnDestroy {
 			}
 		});
 
+		const barHeight = 40;
+
 		setTimeout(() => {
 			this.chartOptions = {
 				series: [
@@ -136,19 +173,24 @@ export class ProcessLifetimeDialogComponent implements OnInit, OnDestroy {
 					},
 				],
 				chart: {
-					height: logsByPID.length * 50 + 100,
+					height: logsByPID.length * barHeight + 100,
 					type: 'rangeBar',
 				},
 				plotOptions: {
 					bar: {
 						horizontal: true,
-						barHeight: '40px',
+						barHeight: barHeight + 'px',
 					},
 				},
 				dataLabels: {
 					enabled: true,
 				},
 			};
+
+			setTimeout(() => {
+				this.changedChartLabels(labelsColors);
+				this.scrollDialogContainerToBottom();
+			}, 0);
 		}, 0);
 	}
 
